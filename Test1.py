48ee0010d8d0b2a80ee5eb605545f744b362d7f8
Aditya <chavanadityawork@gmail.com>
2022-10-03 13:38:34 +0100

    Initial commit

---
+++ Test1.py
@@ -0,0 +1,11 @@
+def add_numbers(a,b):
+    c = a+b
+    return c
+
+value1 = add_numbers(2,3)
+value2 = add_numbers(7,9)
+value3 = add_numbers(97,78)
+
+print("First Value is :", value1)
+print("First Value is :", value2)
+print("First Value is :", value3)
---
+++ Test2.py
@@ -0,0 +1,7 @@
+def print_names(a):
+    for i in range(len(a)):
+        print(a[i])
+
+a = ["Aditya", "Amod", "massimo", "Pierre"]
+print_names(a)
+
