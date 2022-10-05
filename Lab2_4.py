a7f58aadd0a1025e4c0e06d74432a7c58929ee02
Aditya <chavanadityawork@gmail.com>
2022-10-05 11:47:22 +0100

    2nd Commit

---
+++ Lab2_4.py
@@ -0,0 +1,10 @@
+#number of stocks available
+import math
+energy = 24
+metals = 12
+telecom = 11
+
+total_stock = energy + metals + telecom
+print("Percentage of energy stock is : ",round((energy/total_stock)*100), "%")
+print("Percentage of energy stock is : ",round((metals/total_stock)*100), "%")
+print("Percentage of energy stock is : ",round((telecom/total_stock)*100), "%")
