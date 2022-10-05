d9827ed1b28a455ff455243e93bdde6ff83273c2
Aditya <chavanadityawork@gmail.com>
2022-10-05 11:23:55 +0100

    2nd Commit

--- Lab2_1.py
+++ Lab2_1.py
@@ -10,3 +10,11 @@ print(x3)
 print(x4)
 
 print("The values of x1, x2, x3, x4 are: ",x1,",",x2,",",x3,",",x4)
+
+x1,x2,x3,x4= 4*7, 5%27, 3*8*9, 789/43
+
+#print seperately
+print(x1)
+print(x2)
+print(x3)
+print(x4)
